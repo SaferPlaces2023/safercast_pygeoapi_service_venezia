6d2a65234e57e5c3773112ba12ba23a0803643ea
PimpMyGit <tommasoredaelli276@gmail.com>
2025-10-06 17:30:47 +0200

    updates and fixes

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-VERSION = "0.0.6"
+VERSION = "0.0.7"
 PACKAGE_NAME = "directed_pygeoapi_service_venezia"
 AUTHOR = "Valerio Luzzi, Marco Renzi"
 EMAIL = "valerio.luzzi@gecosistema.com, marco.renzi@gecosistema.com"
